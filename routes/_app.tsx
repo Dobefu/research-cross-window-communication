eadc89a981c12f5b20a4074bd593eb3c91fdc235
Connor van Spronssen <info@dobefu.com>
2024-09-29 14:37:16 +0200

    Add a main element to all pages

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,15 +1,19 @@
 import { type PageProps } from "$fresh/server.ts";
+
 export default function App({ Component }: PageProps) {
   return (
     <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>research-cross-window-communication</title>
+        <title>Research - Cross Window Communication</title>
+
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
-        <Component />
+        <main id="main-content">
+          <Component />
+        </main>
       </body>
     </html>
   );
